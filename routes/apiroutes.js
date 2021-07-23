5660d4ce41a7f197644df8ce66209e66c0d71814
Rishil Saxena <rishilnandysaxena@gmail.com>
2021-07-23 13:51:26 -0400

    added css and images folders

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,11 +3,16 @@ const db = require("../models")
 module.exports = function(app){
     app.get("/products/:id", function(req, res){
         db.Product.find({_id: req.params.id}, function(data){
-            res.json(data);
+            res.json(data[0]);
         })
     })
     app.get("/cart/:id", function(req, res){
         db.User.find({_id: req.params.id}).populate("cart").then(function(data){
+            res.json(data);
+        })
+    })
+    app.get("/products/:category", function(req, res){
+        db.Product.find({category: req.params.category}, function(data){
             res.json(data);
         })
     })
